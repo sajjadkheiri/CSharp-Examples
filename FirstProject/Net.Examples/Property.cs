339dd157d8a34fa3b297fe8aac630a41e3f1eecb
sajjad kheiri <sajjadkheiri7@gmail.com>
2024-05-19 22:08:00 +0330

    property

---
+++ FirstProject/Net.Examples/Property.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Net.Examples
+{
+    public class Property
+    {
+        public string FirstName { get; set; } 
+
+
+        // ReadOnly Property
+        public int LastName { get; private set; }
+
+        // OR
+
+        private string _lastName;
+        public string LstName => _lastName;
+
+        //Init-Only Property
+        /// <summary>
+        /// you can just set a value when you initial a class into a initializer
+        /// </summary>
+        public string FatherName { get; init; }
+    }
+}
