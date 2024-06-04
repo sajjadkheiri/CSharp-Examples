9f394729abf5569dfabc4fe1f59bab9de6c96443
sajjad kheiri <sajjadkheiri7@gmail.com>
2024-06-04 14:47:44 +0330

    Continue Generic

---
+++ FirstProject/Net-Example.Generics/Printer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net_Example.Generics
+{
+    /// <summary>
+    /// Without Constraint
+    /// </summary>
+    public class Printer<TModel>
+    {
+        public void PersonPrint(TModel model)
+        {
+            var personModel = model as Person;
+
+            Console.WriteLine(personModel);
+        }
+    }
+
+    /// <summary>
+    /// With Constraint
+    /// Types of constraint => Where T : struct /  
+    ///                        Where T : Class /
+    ///                        Where T : Model (Person) /
+    ///                        Where T : Interface (IPerson) /
+    ///                        Where T : new()
+    /// </summary>
+    public class PersonPrinter<TModel> where TModel : Person
+    {
+        public void PersonPrint(TModel model)
+        {   
+            Console.WriteLine(model);
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class Student : Person
+    {
+        public int StudentId { get; set; }
+    }
+
+    public class Teacher : Person
+    {
+        public int TeacherId { get; set; }
+    }
+
+    public class Car
+    {
+        public int CarId { get; set; }
+    }
+}
