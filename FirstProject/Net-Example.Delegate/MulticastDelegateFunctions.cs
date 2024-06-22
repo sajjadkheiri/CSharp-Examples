398cfa26608d9694e6e7bfdf3d057e11ed0558b9
s.kheiri <s.kheiri@etadbir.com>
2024-06-22 08:52:55 +0330

    Multicast delegate

---
+++ FirstProject/Net-Example.Delegate/MulticastDelegateFunctions.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Net_Example.Delegate
+{
+    /// <summary>
+    /// Multicast delegate should be void because if it has values, you can just get last value.
+    /// Exception in multicast delegate is vital because if in the middle of program has a excpetion
+    /// your delegate will be incomplete
+    /// </summary>
+    public delegate void MethodNamePrinter();
+
+    public class MulticastDelegateFunctions
+    {
+        public void Method1()
+        {
+            Console.WriteLine(nameof(Method1));
+        }
+
+        public void Method2()
+        {
+            Console.WriteLine(nameof(Method1));
+        }
+
+        public void Method3()
+        {
+            Console.WriteLine(nameof(Method1));
+        }
+
+        public void Method4()
+        {
+            Console.WriteLine(nameof(Method1));
+        }
+    }
+}
--- FirstProject/Net-Example.Delegate/Program.cs
+++ FirstProject/Net-Example.Delegate/Program.cs
@@ -19,3 +19,22 @@ PersonToString myDelegate = PersonFullName.GetPersonalFullName;
 var result = myDelegate.Invoke(person);
 
 Console.WriteLine(result);
+
+// Multicast delegate void
+
+MulticastDelegateFunctions multicastDelegateFunctions = new MulticastDelegateFunctions();
+
+MethodNamePrinter myMulticastDelegate = multicastDelegateFunctions.Method1;
+myMulticastDelegate += multicastDelegateFunctions.Method2;
+myMulticastDelegate += multicastDelegateFunctions.Method3;
+myMulticastDelegate += multicastDelegateFunctions.Method4;
+
+myMulticastDelegate();
+
+
+// Multicast delegate with value => Just print the last value
+PersonToString personDelegate = PersonFullName.GetPersonalFullName;
+personDelegate += PersonFullNameReverse.GetPersonalFullName;
+
+PersonPrinter personPrinter = new PersonPrinter();
+personPrinter.Print(personDelegate,person);
