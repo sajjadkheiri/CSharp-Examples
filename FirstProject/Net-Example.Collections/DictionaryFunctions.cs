38b37cc9814b28b9b0fc9decc1b02d7ff8601106
sajjad kheiri <sajjadkheiri7@gmail.com>
2024-06-27 20:31:01 +0330

    Dictionary functions

---
+++ FirstProject/Net-Example.Collections/DictionaryFunctions.cs
@@ -0,0 +1,24 @@
+﻿namespace Net_Example.Collections;
+
+public class DictionaryFunctions
+{
+    Dictionary<string, string> myDic = new Dictionary<string, string>();
+
+    public void Add(string key, string value)
+    {
+        if (!myDic.ContainsKey(key))
+        {
+            myDic.Add(key, value);
+        }
+    }
+
+    public string GetByKey(string key) => myDic[key];
+
+    public void PrintAll()
+    {
+        foreach (var item in myDic)
+        {
+            Console.WriteLine($"The value of {item.Key} is {item.Value}");
+        }
+    }
+}
