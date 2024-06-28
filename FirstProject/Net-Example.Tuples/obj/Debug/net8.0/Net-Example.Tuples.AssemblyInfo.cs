e041aac0dcdb514f138ee0134cd4931f33889a11
sajjad kheiri <sajjadkheiri7@gmail.com>
2024-06-28 20:40:35 +0330

    Exception project

--- FirstProject/Net-Example.Tuples/obj/Debug/net8.0/Net-Example.Tuples.AssemblyInfo.cs
+++ FirstProject/Net-Example.Tuples/obj/Debug/net8.0/Net-Example.Tuples.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Net-Example.Tuples")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44447398ff96d02be76b18abe772e505a2d1635a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c13dd1c69c313e0c86d4d8375d6e2e7247d7d2f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Net-Example.Tuples")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Net-Example.Tuples")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
