f052c7dbcd06bebf9727fa580865aad9be8d18a9
sajjad kheiri <sajjadkheiri7@gmail.com>
2024-06-30 21:12:27 +0330

    Exception handling

--- FirstProject/Net-Example.Exceptions/ExceptionFunctions.cs
+++ FirstProject/Net-Example.Exceptions/ExceptionFunctions.cs
@@ -2,5 +2,39 @@
 
 public class ExceptionFunctions
 {
-    
+    /// <summary>
+    /// when : when is a condition and you can use 2 or more similar catch with different conditions
+    /// </summary>
+    public void Sum(int a, int b)
+    {
+        try
+        {
+            if (a < default(int))
+            {
+                throw new Exception("");
+            }
+
+            if (b < default(int))
+            {
+                throw new FormatException("");
+            }
+        }
+        catch (FormatException fe) when (!string.IsNullOrEmpty(fe.Source))
+        {
+            Console.WriteLine($"Format Exception : {fe.Message}");
+            Console.WriteLine($"Format Exception Source : {fe.Source}");
+        }
+        catch (FormatException fe) when (string.IsNullOrEmpty(fe.Source))
+        {
+            Console.WriteLine($"Format Exception : {fe.Message}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Exception : {e.Message}");
+        }
+        finally
+        {
+            Console.WriteLine("End");
+        }
+    }
 }
