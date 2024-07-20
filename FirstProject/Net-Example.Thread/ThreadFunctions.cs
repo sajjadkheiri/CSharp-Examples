1d996af7c60c7fd2ecefa2b9729915576273b967
sajjad kheiri <sajjadkheiri7@gmail.com>
2024-07-20 22:19:56 +0330

    Join and sleep thread

--- FirstProject/Net-Example.Thread/ThreadFunctions.cs
+++ FirstProject/Net-Example.Thread/ThreadFunctions.cs
@@ -26,4 +26,55 @@ public class ThreadFunctions
 
         Console.ReadKey();
     }
+
+    /// <summary>
+    /// The join method which allows one thread to wait until another thread completes its execution
+    /// </summary>
+    public void JoinThread()
+    {
+        CharPrinter charPrinter = new CharPrinter();
+        System.Threading.Thread thread = new(charPrinter.DashPrinter);
+
+        thread.Start();
+        var isFinished = thread.Join(30);
+
+        Console.WriteLine(isFinished);
+
+        charPrinter.StarPrinter();
+
+        Console.ReadLine();
+    }
+
+    public void SleepThread()
+    {
+        CharPrinter charPrinter = new CharPrinter();
+        System.Threading.Thread thread = new(charPrinter.DashPrinter);
+
+        thread.Start();
+
+        System.Threading.Thread.Sleep(TimeSpan.FromSeconds(2));
+
+        charPrinter.StarPrinter();
+
+        Console.ReadLine();
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void YieldThread()
+    {
+        CharPrinter charPrinter = new CharPrinter();
+        System.Threading.Thread thread = new(charPrinter.DashPrinter);
+
+        thread.Start();
+
+        System.Threading.Thread.Yield();
+        System.Threading.Thread.Sleep(0);
+
+        charPrinter.StarPrinter();
+
+        Console.ReadLine();
+    }
 }
