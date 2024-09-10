5d0e384240df3681588331033e98329d257ff7eb
sajjad kheiri <sajjadkheiri7@gmail.com>
2024-09-10 20:35:27 +0330

    git ignore

--- FirstProject/Net-Example.Enums/obj/Debug/net8.0/Net-Example.Enums.AssemblyInfo.cs
+++ FirstProject/Net-Example.Enums/obj/Debug/net8.0/Net-Example.Enums.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Net-Example.Enums")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caed44cc36aae245d081ff7c0796945c12dd05ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a43950271a9e540be1e2f7eb5bc3e23d302ae1a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Net-Example.Enums")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Net-Example.Enums")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
